b5cca86d6f2cc3913d709a9005b63f7a17e5285a
Sruthi192005 <220101131083@cutm.ac.in>
2025-08-18 18:58:22 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,13 +1,23 @@
 `timescale 1ns / 1ps
 
 module tt_um_plc_prg (
-    input  wire clk,
-    input  wire reset,
-    input  wire start,
-    input  wire AUTO,
-    input  wire MAN,
-    output reg  Control
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (1=output)
+    input  wire       ena,      // always 1 when your design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // async active-low reset
 );
+
+    // Map UI bits
+    wire reset = ~rst_n;   // active high reset internally
+    wire start = ui_in[0];
+    wire AUTO  = ui_in[1];
+    wire MAN   = ui_in[2];
+
+    reg Control;
 
     // Timer preset (different in simulation vs hardware)
 `ifdef COCOTB_SIM
@@ -24,7 +34,7 @@ module tt_um_plc_prg (
             counter     <= 0;
             timer_done  <= 0;
             Control     <= 0;
-        end else begin
+        end else if (ena) begin
             if (AUTO && start) begin
                 // Timer ON delay
                 if (!timer_done) begin
@@ -48,5 +58,12 @@ module tt_um_plc_prg (
         end
     end
 
-endmodule
+    // Map output
+    assign uo_out[0] = Control;  // only using bit[0]
+    assign uo_out[7:1] = 7'b0;
 
+    // Not using bidirectional IOs
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
+
+endmodule
